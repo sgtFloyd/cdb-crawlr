871a24d0f3607f05e5f163c326d14ce4fc48eb2e
sgtFloyd <sgt.floydpepper@gmail.com>
2012-11-01 19:19:07 -0400

    bump version

--- lib/cdb-crawlr.rb
+++ lib/cdb-crawlr.rb
@@ -8,7 +8,7 @@ require 'cdb/issue'
 require 'cdb/title'
 
 module CDB
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 
   BASE_URL = 'http://www.comicbookdb.com'
   REQUEST_HEADERS = {'Connection' => 'keep-alive'}
